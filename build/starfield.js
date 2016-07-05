ed15380ae6c46d037ac614db2cbf8a0b4f5675c7
David Vicklund <david@vicklund.com>
2016-07-05 15:43:05 -0700

    add stars and circle drawing

---
+++ build/starfield.js
@@ -0,0 +1,135 @@
+var starColors = ['#FF2000', '#FFD700', '#800000', '#00FFFF', '#FFFFE0', '#FF8C00'];
+var colorCount = starColors.length;
+
+function Star(x, y, size, velocity, color) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.velocity = velocity;
+  this.color = color;
+}
+
+function Starfield() {
+  this.fps = 30;
+  this.canvas = null;
+  this.width = 0;
+  this.height = 0;
+  this.minVelocity = 15;
+  this.maxVelocity = 100;
+  this.maxStarSize = 4;
+  this.starCount = 300;
+  this.stars = [];
+  this.intervalId = 0;
+  this.dragging = false;
+  this.dragStart = null;
+  this.mousePos = null;
+}
+
+Starfield.prototype.init = function(div) {
+  var self = this;
+
+  this.containerDiv = div;
+  self.width = window.innerWidth;
+  self.height = window.innerHeight;
+
+  window.addEventListener('resize', function resize(event) {
+    self.width = window.innerWidth;
+    self.height = window.innerHeight;
+    self.canvas.width = self.width;
+    self.canvas.height = self.height;
+    self.draw();
+  })
+
+  var canvas = document.createElement('canvas');
+  div.appendChild(canvas)
+  this.canvas = canvas
+  this.canvas.width = this.width
+  this.canvas.height = this.height
+
+  function move(e) {
+    self.mousePos = e
+  }
+
+  window.addEventListener('mousedown', function(e) {
+    self.dragging = true
+    self.dragStart = e
+    window.addEventListener('mousemove', move)
+  })
+
+  window.addEventListener('mouseup', function(e) {
+    self.dragging = false
+    self.dropStars();
+    self.dragStart = null
+    self.mousePos = null
+    window.removeEventListener('mousemove', move)
+  })
+}
+
+Starfield.prototype.start = function() {
+  var stars = [];
+
+  for(var star = 0; star < this.starCount; star++) {
+    stars[star] = new Star(Math.random() * this.width,
+                           Math.random() * this.height,
+                           Math.random() * this.maxStarSize + 1,
+                           (Math.random() * (this.maxVelocity - this.minVelocity)) + this.minVelocity,
+                           starColors[Math.floor(Math.random() * colorCount)])
+  }
+
+  this.stars = stars;
+
+  var self = this;
+  this.intervalId = setInterval(function() {
+    self.update()
+    self.draw()
+  }, 1000 / this.fps)
+}
+
+Starfield.prototype.update = function() {
+  var dt = 1/this.fps;
+  for(var i = 0; i < this.stars.length; i++) {
+    var star = this.stars[i]
+    star.y += dt * star.velocity
+    if(star.y > this.height) {
+      this.stars[i] = new Star(Math.random() * this.width,
+                               0,
+                               Math.random() * this.maxStarSize + 1,
+                               (Math.random() * (this.maxVelocity - this.minVelocity)) + this.minVelocity,
+                               starColors[Math.floor(Math.random() * colorCount)])
+    }
+  }
+}
+
+Starfield.prototype.draw = function() {
+  var ctx = this.canvas.getContext('2d')
+
+  ctx.fillStyle = '#000000'
+  ctx.fillRect(0, 0, this.width, this.height)
+
+  for(var i = 0; i < this.stars.length; i++) {
+    var star = this.stars[i]
+    ctx.fillStyle = star.color
+    ctx.beginPath()
+    ctx.arc(star.x, star.y, star.size, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  if(this.dragging && this.mousePos) {
+    ctx.strokeStyle = '#FFFFFF'
+    ctx.beginPath()
+    ctx.arc(
+      ((this.mousePos.x - this.dragStart.x)/2) + this.dragStart.x,
+      ((this.mousePos.y - this.dragStart.y)/2) + this.dragStart.y,
+      Math.max(Math.abs(this.mousePos.x - this.dragStart.x)/2, Math.abs(this.mousePos.y - this.dragStart.y)/2),
+      0, 2*Math.PI)
+    ctx.stroke()
+  }
+}
+
+Starfield.prototype.dropStars = function() {
+  for(var i = 0; i < this.stars.length; i++) {
+    if(this.stars[i].x) {
+
+    }
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp')
+var clean = require('gulp-clean')
+var webpack = require('webpack-stream')
+var sass = require('gulp-sass')
+var minifyCss = require('gulp-cssnano')
+var sourcemaps = require('gulp-sourcemaps')
+var autoprefixer = require('gulp-autoprefixer')
+var protractor = require('gulp-protractor').protractor
+var mocha = require('gulp-mocha')
+
+var paths = {
+  css: ['app/**/*.scss', 'app/**/*.sass', 'app/*.css'],
+  html: ['app/**/*.html'],
+  js: ['app/**/*.js'],
+  static: ['img/*', 'vendor/*'],
+  testBack: ['test/*Test.js'],
+  testFront: ['test/specIndex.js']
+}
+
+gulp.task('build:css', function() {
+  gulp.src('app/scss/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'))
+
+  gulp.src('app/style.css')
+    .pipe(gulp.dest('build/'))
+})
+
+gulp.task('build:html', function() {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'))
+})
+
+gulp.task('build:js', function() {
+  gulp.src('app/js/*.js').pipe(gulp.dest('build/'))
+
+  // return gulp.src('app/js/entry.js')
+  // .pipe(webpack({
+  //   output: {
+  //     filename: 'bundle.js'
+  //   }
+  // }))
+  // .pipe(gulp.dest('build/'))
+})
+
+gulp.task('build:static', function() {
+  gulp.src('img/*')
+  .pipe(gulp.dest('build/img'))
+
+  gulp.src('vendor/*')
+  .pipe(gulp.dest('build/vendor'))
+})
+
+gulp.task('clean', function() {
+  return gulp.src('build', {read: false})
+        .pipe(clean({force: true}))
+})
+
+gulp.task('test:back', function() {
+  return gulp.src(paths.testBack, {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+})
+
+gulp.task('test:front', function() {
+  gulp.src(paths.testFront).pipe(protractor({
+    configFile: 'protractor.config.js'
+  }))
+  .on('error', function(e) {throw e})
+})
+
+gulp.task('watch:css', function() {
+	gulp.watch(paths.css, ['build:css'])
+})
+
+gulp.task('watch:html', function() {
+	gulp.watch(paths.html, ['build:html'])
+})
+
+gulp.task('watch:js', function() {
+	gulp.watch(paths.js, ['build:js'])
+})
+
+gulp.task('watch:static', function() {
+  gulp.watch(paths.static, ['build:static'])
+})
+
+gulp.task('build:all', ['build:css', 'build:html', 'build:js', 'build:static'])
+gulp.task('test:all', ['test:back', 'test:front'])
+gulp.task('watch:all', ['watch:css', 'watch:html', 'watch:js'])
+gulp.task('default', ['build:all', 'watch:all'])
